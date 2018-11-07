5bf0895171bbcb738fbea3aa24e796a12d966224
hebb-zbc <hebb@zbc.dk>
2018-11-07 15:02:03 +0100

    small change

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-window = Gtk.Window(title="Hello World")
+window = Gtk.Window(title="Hello Github...")
 window.show()
 window.connect("destroy", Gtk.main_quit)
 Gtk.main()
