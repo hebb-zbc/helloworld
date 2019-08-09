d6b61309d1a634aca4815d117e3d3e87775bb65c
hebb-zbc <hebb@zbc.dk>
2019-08-09 11:27:54 +0200

    lortegit...

--- hello.py
+++ hello.py
@@ -4,6 +4,8 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+print("udvikling...")
+
 window = Gtk.Window(title="Hello Github...")
 window.show()
 window.connect("destroy", Gtk.main_quit)
