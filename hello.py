1fb29e7cfa470e67ca348c7cad0cc50389ce71b6
hebb-zbc <hebb@zbc.dk>
2019-08-13 07:59:32 +0200

    Revert "udkommenteret vindue"
    This reverts commit 0a1fcbce33467e703cfc1fe2fa0d0612c042b68e.
    
    Lortekode :)

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-#import gi
-#gi.require_version("Gtk", "3.0")
-#from gi.repository import Gtk
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
 
-print("udvikling... ?")
+print("udvikling...")
 
-#window = Gtk.Window(title="Hello Github...")
-#window.show()
-#window.connect("destroy", Gtk.main_quit)
-#Gtk.main()
+window = Gtk.Window(title="Hello Github...")
+window.show()
+window.connect("destroy", Gtk.main_quit)
+Gtk.main()
 
