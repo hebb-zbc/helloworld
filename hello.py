b6d47a894744fbad052bdb627cdec512f542f71c
hebb-zbc <hebb@zbc.dk>
2019-08-14 15:37:21 +0200

    test af atom

--- hello.py
+++ hello.py
@@ -7,9 +7,9 @@
 print("udvikling...")
 print("ja...")
 print("også inde fra borgen")
+print("udvikling i atom")
 
 #window = Gtk.Window(title="Hello Github...")
 #window.show()
 #window.connect("destroy", Gtk.main_quit)
 #Gtk.main()
-
